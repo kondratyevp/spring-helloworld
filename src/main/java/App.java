424a8f29b15b34318cb646fef9f20a4811f39156
pavelkondratyev <pavel-npk@yandex.ru>
2021-08-03 23:23:50 +0500

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,19 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        boolean compareHelloWorld = bean == bean2;
+        System.out.println(compareHelloWorld);
+        System.out.println(bean);
+        System.out.println(bean2);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        boolean compareCat = cat1 == cat2;
+        System.out.println(compareCat);
+        System.out.println(cat1);
+        System.out.println(cat2);
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String name;
+
+    public String getname() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
